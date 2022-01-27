b45e6f5bd989b861142b7dbc9f17e89a2db08b6a
Bryan Vela <55074395+bdvela@users.noreply.github.com>
2022-01-26 21:09:53 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-const sectionsPerPage = new fullpage('#fullpage', {});
+const sectionsPerPage = new fullpage('#fullpage', {
+    autoScrolling: true,
+    fitToSection: false,
+    fitToSectionDelay: 300,
+    easing: 'easeInOutCubic',
+    scrollingSpeed: 700,
+    css3: true,
+    easingcss3: 'ease-out',
+    loopBottom: false,
+
+    navigation: true,
+    menu: '#menu',
+    anchors: ['inicio', 'info', 'precios', 'contacto'],
+    navigationTooltips: ['Inicio', 'Info', 'Precios', 'Contacto'],
+    showActiveTooltip: false,
+});
